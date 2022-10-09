4ac454a433d0af93129c21ffc5f92f6b52453f44
tanmay013 <59093235+tanmay013@users.noreply.github.com>
2022-10-09 18:45:26 +0530

    first

---
+++ precache-manifest.1b50ca663fbe92167e1f565c96f85378.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6a8cd90f0b18d436168a",
+    "url": "/static/css/main.5f00f132.chunk.css"
+  },
+  {
+    "revision": "6a8cd90f0b18d436168a",
+    "url": "/static/js/main.6a8cd90f.chunk.js"
+  },
+  {
+    "revision": "24758a8b680d33a928be",
+    "url": "/static/js/1.24758a8b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "156b43fae28a86b2c1894c656b2bfcce",
+    "url": "/index.html"
+  }
+];
